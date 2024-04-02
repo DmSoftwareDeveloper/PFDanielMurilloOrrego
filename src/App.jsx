721e52b20de70bd08a957b5bc625335820454d92
Daniel Murillo <Daniel_Sistematic10@hotmail.com>
2024-04-01 19:16:46 -0500

    Se hacen cambios del diseño para el proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
+import Checkout from './components/Checkout/Checkout'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 
@@ -18,6 +19,7 @@ function App() {
               <Route path="/category/:categoryId" element={<ItemListContainer greeting={"PRODUCTOS POR CATEGORIA"} />} />
               <Route path="/item/:itemId" element={<ItemDetailContainer />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
               <Route path="*" element={<h1>404 NOT FOUND</h1>} />
             </Routes>
           </CartProvider>
